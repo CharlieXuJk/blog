699ac560fd644b45cc45c1b764047328649bd146
dell <hans040515@gmail.com>
2021-08-29 17:32:15 -0700

    complete the header layout(maily for the position)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
+
+import 'antd/dist/antd.css'
 import '../styles/globals.css'
 import App from 'next/app'
-import 'antd/dist/antd.css'
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
